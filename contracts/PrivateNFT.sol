a6da1e4a97ee2856b217539efac863f80bf56b78
joanseskinestelleh <joanseskinestellehj4c@hotmail.com>
2024-09-19 08:42:57 +0000

    Initialised

---
+++ contracts/PrivateNFT.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC721("72a8967724ae48a3523deda0219704998681fa90e290b8886853f99bb969a0ff","72a8967724ae48a3523deda0219704998681fa90e290b8886853f99bb969a0ff")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function balanceOf(address owner) public view override returns (uint256) {
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return super.balanceOf(owner);
+    }
+
+    function ownerOf(uint256 tokenId) public view override returns (address) {
+        address owner = super.ownerOf(tokenId);
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return owner;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        address owner = super.ownerOf(tokenId);
+        require(msg.sender == owner, "PrivateNFT: msg.sender != owner");
+        return super.tokenURI(tokenId);
+    }
+}
